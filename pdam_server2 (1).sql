5e0d60e3d840ad9d6b5cdd799a93a78842559102
Hendra Gunawan <hendragunz@codecampz.com>
2019-01-15 21:25:34 +0700

    backup db

--- pdam_server2 (1).sql
+++ pdam_server2 (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Dec 26, 2018 at 01:07 PM
+-- Generation Time: Jan 15, 2019 at 03:25 PM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.1.25
 
@@ -25,6 +25,73 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `beritas`
+--
+
+CREATE TABLE `beritas` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `category_id` int(10) UNSIGNED DEFAULT NULL,
+  `content` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `judul` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `beritas`
+--
+
+INSERT INTO `beritas` (`id`, `category_id`, `content`, `judul`, `created_at`, `updated_at`) VALUES
+(1, 1, 'Pelanggan yang terhormat.. Selamat datang di PDAM Kertawinangun, Kami harap dengan adanya sistem informasi Pemakaian air menjadikan mutu layanan PDAM di Kertawinangun bisa membangun kualitas dan layanan untuk seluruh pelanggan.', 'Selamat datang di PDAM Kertawinangun', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`, `created_at`, `updated_at`) VALUES
+(1, 'Berita', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `histories`
+--
+
+CREATE TABLE `histories` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `user_id` int(10) UNSIGNED DEFAULT NULL,
+  `bulan` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tahun` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `jumlah_pemakaian` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `foto_meteran` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `total_bayar` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `histories`
+--
+
+INSERT INTO `histories` (`id`, `user_id`, `bulan`, `tahun`, `jumlah_pemakaian`, `foto_meteran`, `total_bayar`, `created_at`, `updated_at`) VALUES
+(3, 1, '11', '2018', '10', '1120181.png', '3000', '2019-01-15 07:04:14', '2019-01-15 07:04:14');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `migrations`
 --
 
@@ -42,7 +109,9 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (1, '2014_10_12_000000_create_users_table', 1),
 (2, '2014_10_12_100000_create_password_resets_table', 1),
 (3, '2018_08_03_114156_create_urls_table', 1),
-(4, '2018_09_04_124737_create_permission_tables', 1);
+(4, '2018_09_04_124737_create_permission_tables', 1),
+(5, '2018_12_26_190807_add_some_information_into_users_table', 1),
+(6, '2019_01_15_190807_create_histories_table', 1);
 
 -- --------------------------------------------------------
 
@@ -87,6 +156,13 @@ CREATE TABLE `password_resets` (
   `created_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `password_resets`
+--
+
+INSERT INTO `password_resets` (`email`, `token`, `created_at`) VALUES
+('hilmysyarif@gmail.com', '$2y$10$27CvOkfFszkSyfZHZ/qmRO18gpA9itjITsEQucOIA4czTMhQPbC5i', '2019-01-15 03:44:51');
+
 -- --------------------------------------------------------
 
 --
@@ -100,13 +176,6 @@ CREATE TABLE `permissions` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `permissions`
---
-
-INSERT INTO `permissions` (`id`, `name`, `guard_name`, `created_at`, `updated_at`) VALUES
-(1, 'admin', 'web', '2018-12-25 09:18:17', '2018-12-25 09:18:17');
 
 -- --------------------------------------------------------
 
@@ -127,7 +196,7 @@ CREATE TABLE `roles` (
 --
 
 INSERT INTO `roles` (`id`, `name`, `guard_name`, `created_at`, `updated_at`) VALUES
-(1, 'admin', 'web', '2018-12-25 09:18:17', '2018-12-25 09:18:17');
+(1, 'admin', 'Admin', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -139,13 +208,6 @@ CREATE TABLE `role_has_permissions` (
   `permission_id` int(10) UNSIGNED NOT NULL,
   `role_id` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `role_has_permissions`
---
-
-INSERT INTO `role_has_permissions` (`permission_id`, `role_id`) VALUES
-(1, 1);
 
 -- --------------------------------------------------------
 
@@ -166,14 +228,6 @@ CREATE TABLE `urls` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Dumping data for table `urls`
---
-
-INSERT INTO `urls` (`id`, `user_id`, `url_key`, `is_custom`, `long_url`, `meta_title`, `clicks`, `ip`, `created_at`, `updated_at`) VALUES
-(1, 1, 'test', 1, 'https://google.com', 'Google', 1, '127.0.0.1', '2018-12-25 09:19:25', '2018-12-25 09:20:17'),
-(2, 1, 'YPEY15', 0, 'https://google.com', 'Google', 1, '127.0.0.1', '2018-12-25 10:08:34', '2018-12-25 10:08:42');
-
 -- --------------------------------------------------------
 
 --
@@ -188,20 +242,43 @@ CREATE TABLE `users` (
   `password` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `first_name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `last_name` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `id_pelanggan` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `alamat` text COLLATE utf8mb4_unicode_ci,
+  `no_hp` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'admin', 'admin@newt.test', NULL, '$2y$10$FZpyGj9Yq1brKWbJ0DSfg.YuM4jAg1TFUvhsNabS1Akmz8itLsrv.', NULL, '2018-12-25 09:18:16', '2018-12-25 09:18:16'),
-(2, 'user', 'user@newt.test', NULL, '$2y$10$mGMDT3eWsGI1DhrHIgI.S.X4uQV6qFpgT.hc/MczcibqNWrvplLP.', NULL, '2018-12-25 09:18:16', '2018-12-25 09:18:16');
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`, `first_name`, `last_name`, `id_pelanggan`, `alamat`, `no_hp`) VALUES
+(1, 'hilmysyarif', 'hilmysyarif@gmail.com', NULL, '$2y$10$3BIorlW6UiYWcJQvQ577.uL4wFJoKhrFwzse5ZCwrypdBWC.h3frW', 'MY0uRe9v3GW9814zEw7M7fUxiwvGNgzm7UDbZObZyudHzKw0k4SrrOqnB964', '2019-01-15 03:41:22', '2019-01-15 03:41:57', 'hilmy', 'syarif', '12345', 'perumahan pondok aren indah, jl. akasia iii blok b3/6', '0895365359636');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `beritas`
+--
+ALTER TABLE `beritas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `histories`
+--
+ALTER TABLE `histories`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `histories_user_id_foreign` (`user_id`);
 
 --
 -- Indexes for table `migrations`
@@ -268,16 +345,34 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `beritas`
+--
+ALTER TABLE `beritas`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `histories`
+--
+ALTER TABLE `histories`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `permissions`
 --
 ALTER TABLE `permissions`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `roles`
@@ -289,17 +384,23 @@ ALTER TABLE `roles`
 -- AUTO_INCREMENT for table `urls`
 --
 ALTER TABLE `urls`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `histories`
+--
+ALTER TABLE `histories`
+  ADD CONSTRAINT `histories_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `model_has_permissions`
